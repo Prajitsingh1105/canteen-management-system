4a55b087840ad8b1ca612e28a587228829dc959f
umang0903 <umangrana2020@gmail.com>
2026-03-26 10:00:52 +0530

    Added menuitems schema

---
+++ server/models/MenuItem.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const menuItemSchema = new mongoose.Schema(
+  {
+
+    name: {
+      type: String,
+      required: true
+    },
+
+    price: {
+      type: Number,
+      required: true
+    },
+
+    image: {
+      type: String,
+      required: true
+    },
+
+    category: {
+      type: String,
+      enum: ["Snacks", "Drinks", "Meals", "Desserts"],
+      required: true
+    },
+
+    isAvailable: {
+      type: Boolean,
+      default: true
+    }
+
+  },
+  {
+    timestamps: true
+  }
+);
+
+const MenuItem = mongoose.model("MenuItem", menuItemSchema);
+
+export default MenuItem;
