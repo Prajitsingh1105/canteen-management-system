c486d81a74008d808e70a3739d48158b0fb08e28
umang0903 <umangrana2020@gmail.com>
2026-03-26 10:07:59 +0530

    added auth middleware

---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,69 @@
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
+
+// PROTECT 
+
+export const protect = async (req, res, next) => {
+  try {
+    let token;
+
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith("Bearer")
+    ) {
+      token = req.headers.authorization.split(" ")[1];
+    }
+
+    if (!token) {
+      return res.status(401).json({
+        message: "Not authorized, token missing"
+      });
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = await User.findById(decoded.id).select("-password");
+
+    if (!user) {
+      return res.status(401).json({
+        message: "User not found"
+      });
+    }
+
+    req.user = user;
+
+    next();
+
+  } catch (error) {
+    res.status(401).json({
+      message: "Invalid token"
+    });
+  }
+};
+
+
+// ADMIN ONLY
+
+export const adminOnly = (req, res, next) => {
+  if (req.user && req.user.role === "admin") {
+    next();
+  } else {
+    res.status(403).json({
+      message: "Access denied: Admin only"
+    });
+  }
+};
+
+
+//STUDENT ONLY 
+
+export const studentOnly = (req, res, next) => {
+  if (req.user && req.user.role === "student") {
+    next();
+  } else {
+    res.status(403).json({
+      message: "Access denied: Students only"
+    });
+  }
+};
