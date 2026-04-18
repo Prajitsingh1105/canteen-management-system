9d523aac22f2a49a0fdcf96acc78e40d02fe1c80
Prajitsingh1105 <prajitsingh05@gmail.com>
2026-04-19 04:12:28 +0530

    updated server

--- server/server.js
+++ server/server.js
@@ -1,8 +1,7 @@
-
 import dotenv from "dotenv";
 dotenv.config();
-import express from "express";
 
+import express from "express";
 import cors from "cors";
 
 import connectDB from "./config/db.js";
@@ -12,45 +11,47 @@ import menuRoutes from "./routes/menuRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
 import analyticsRoutes from "./routes/analyticsRoutes.js";
 
-
-
 connectDB();
 
 const app = express();
 
-app.use(cors({
-  origin: [
-    "http://flasheats-client.s3-website-us-east-1.amazonaws.com",
-    "http://localhost:5173"
-  ],
-  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-}));
-app.options('*', cors());
+
+app.use(
+  cors({
+    origin: [
+      "http://flasheats-client.s3-website-us-east-1.amazonaws.com",
+      "http://localhost:5173",
+    ],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+
+app.options("*", cors());
+
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
 
 app.use("/api/auth", authRoutes);
 app.use("/api/menu", menuRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/analytics", analyticsRoutes);
 
+
 app.get("/", (req, res) => {
   res.send("Canteen Management API Running");
 });
+
 app.get("/api/test", (req, res) => {
   res.json({ working: true });
 });
 
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, "0.0.0.0", () =>
-  console.log(`Server running on port ${PORT}`)
-);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
