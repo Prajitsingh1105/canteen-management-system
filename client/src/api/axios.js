58c352ef8bbc54ba0cce92f296e1c782f397bb2b
Prajitsingh1105 <prajitsingh05@gmail.com>
2026-04-19 15:34:55 +0530

    updated axios.js

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -6,10 +6,20 @@ const API = axios.create({
 
 // Attach JWT token automatically
 API.interceptors.request.use((req) => {
-  const user = JSON.parse(localStorage.getItem("user"));
+  const userStr = localStorage.getItem("user");
 
-  if (user?.token) {
-    req.headers.Authorization = `Bearer ${user.token}`;
+  let user = null;
+  try {
+    user = userStr ? JSON.parse(userStr) : null;
+  } catch (err) {
+    localStorage.removeItem("user");
+    user = null;
+  }
+
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    req.headers.Authorization = `Bearer ${token}`;
   }
 
   return req;
